dfce010555dee9f7abf356df9a522b6464630a95
Matt Lewis <matt.lewis@ibm.com>
2025-11-11 11:20:34 +0000

    remove calling out errors

--- test_data/broken_calculator.py
+++ test_data/broken_calculator.py
@@ -1,8 +1,3 @@
-"""
-Calculator with a couple of errors to test agent self-correction.
-The agent should detect these errors and fix them.
-"""
-
 class Calculator:
     """Simple calculator class"""
     
@@ -13,7 +8,6 @@ class Calculator:
         return x + y
     
     def subtract(self, x, y):
-        # Error 1: Using undefined variables 'a' and 'b' instead of 'x' and 'y'
         return a - b
     
     def multiply(self, x, y):
@@ -25,7 +19,6 @@ class Calculator:
         return x / y
     
     def power(self, x, y):
-        # Error 2: Using undefined function 'pow_function' instead of built-in 'pow'
         return pow_function(x, y)
 
 
